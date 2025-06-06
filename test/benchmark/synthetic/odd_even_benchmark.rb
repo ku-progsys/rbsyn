01f4576174565d684a22c65a454476d93ea992b4
LegendaryPenguin <nischayrawal2006@gmail.com>
2025-06-05 23:18:49 -0500

    odd/even benchmark (#44)

---
+++ test/benchmark/synthetic/odd_even_benchmark.rb
@@ -0,0 +1,39 @@
+require "test_helper"
+
+describe "Synthetic" do
+  it "checks numbers are odd/even" do
+    RDL.nowrap :Integer
+    RDL.nowrap :BasicObject
+    RDL.type :Integer, :%, "(Integer) -> Integer"
+    RDL.type :Integer, :!=, "(Integer) -> %bool"
+    RDL.type :Integer, :==, "(Integer) -> %bool"
+    RDL.type :BasicObject, :!, '() -> %bool'
+    RDL.nowrap :Hash
+    RDL.type_params :Hash, [:k, :v], :all?
+
+    define :even?, "(Integer) -> %bool", [], consts: true do
+
+      spec "returns true for even" do
+        setup {
+          even? 4
+        }
+
+        post { |result|
+          assert { result == true }
+        }
+      end
+
+      spec "returns false for odd" do
+        setup {
+          even? 5
+        }
+
+        post { |result|
+          assert { result == false }
+        }
+      end
+
+      generate_program
+    end
+  end
+end
